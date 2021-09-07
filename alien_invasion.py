4061a344e4abd77ec7ceefd0119a5363911ea019
Dipjyoty Pandit <dipjyotypandit@gmail.com>
2021-09-07 13:59:51 +0530

    Refactored alien_invasion

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,12 +21,6 @@ def run_game():
     # Create the fleet of Aliens.
     gf.create_fleet(ai_settings, screen, aliens)
 
-    # Set the background color
-    bg_color = (230, 230, 230)
-
-    #Make an alien.
-    # alien = Alien(ai_settings, screen)
-
     # Start the main loop for the game
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
